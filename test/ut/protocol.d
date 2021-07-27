d50593e0bd0260f305bf496c1b92d8007afec22f
Sebastiaan Koppe <mail@skoppe.eu>
2021-07-27 21:49:59 +0200

    Add test for space-less signature header

--- test/ut/protocol.d
+++ test/ut/protocol.d
@@ -88,6 +88,15 @@ pure @safe unittest {
   request.extractSignatureHeader.get.should == SignatureHeader("AWS4-HMAC-SHA256", Credential("test", "20210716", "us-east-1", "s3", "aws4_request"), ["host", "x-amz-content-sha256", "x-amz-date"], "b94e6180c3db76255cd36db1b82b7872b6560e3132128f0982c5d1b96162c59b");
 }
 
+enum testRequestNoSpace = cast(ubyte[])"GET /test-bucket/?delimiter=%2F&prefix=&encoding-type=url&max-keys=100&list-type=2 HTTP/1.1\r\nUser-Agent: dlang-requests\r\nx-amz-content-sha256: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855\r\nAccept-Encoding: gzip,deflate\r\nhost: 0.0.0.0:39239\r\nx-amz-date: 20210716T085824Z\r\nConnection: Keep-Alive\r\nauthorization: AWS4-HMAC-SHA256 Credential=test/20210716/us-east-1/s3/aws4_request,SignedHeaders=host;x-amz-content-sha256;x-amz-date,Signature=b94e6180c3db76255cd36db1b82b7872b6560e3132128f0982c5d1b96162c59b\r\n\r\n";
+
+@("extractSignatureHeader.no-space")
+pure @safe unittest {
+  import s3proxy.http : parseHttpRequest;
+  auto request = testRequestNoSpace.parseHttpRequest();
+
+  request.extractSignatureHeader.get.should == SignatureHeader("AWS4-HMAC-SHA256", Credential("test", "20210716", "us-east-1", "s3", "aws4_request"), ["host", "x-amz-content-sha256", "x-amz-date"], "b94e6180c3db76255cd36db1b82b7872b6560e3132128f0982c5d1b96162c59b");
+}
 @("extractCanonicalRequest")
 pure @safe unittest {
   import aws.sigv4 : CanonicalRequest;
