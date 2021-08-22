2995559f8ea134eebe6fd70ea7a19caf5d807d92
Sebastiaan Koppe <mail@skoppe.eu>
2021-08-22 13:26:39 +0200

    Refactor app.d

--- source/app.d
+++ source/app.d
@@ -1,33 +1,50 @@
 module app;
 
-@safe void main() {
-  import concurrency.stream : transform, take, toList;
-  import concurrency.sender;
-  import concurrency.operations : via, then, whenAll, withStopToken;
-  import concurrency.thread;
-  import concurrency;
-  import concurrency.nursery;
-  import s3proxy.proxy;
-  import s3proxy.http;
-  import s3proxy.server;
-  import s3proxy.config;
-  import std.experimental.logger : globalLogLevel;
-  import std.file : readText;
+import s3proxy.server : socket_t;
+import s3proxy.proxy : Proxy;
 
-  auto socket = openListeningSocket("0.0.0.0", 8080).unwrap();
-  auto server = listenServer(socket);
+@safe void main() {
+  import concurrency.thread : stdTaskPool;
+  import concurrency : syncWait;
+  import concurrency.nursery : Nursery;
+
+  auto server = createServer("0.0.0.0", 8080);
   auto pool = cast(shared)stdTaskPool(8);
   auto nursery = new shared Nursery();
 
-  auto config = loadConfig(readText("s3proxy.toml")).parseConfig();
-  auto api = shared Proxy(config);
+  auto api = createProxy("s3proxy.toml");
 
-  globalLogLevel = config.logging;
-
-  auto runServer = server.collect((socket_t t) shared @trusted {
-      nursery.run(just(t).via(pool.getScheduler().schedule()).withStopToken(&api.handle));
+  auto runServer = server.collect((socket_t socket) shared @trusted {
+      auto task = api.handleRequest(socket, pool.getScheduler);
+      nursery.run(task);
     });
 
   nursery.run(runServer);
   nursery.syncWait().assumeOk;
+}
+
+auto handleRequest(Scheduler)(ref shared Proxy api, socket_t t, Scheduler scheduler) {
+  import concurrency.sender : just;
+  import concurrency.operations : via, withStopToken;
+  return just(t).via(scheduler.schedule()).withStopToken(&api.handle);
+}
+
+auto createServer(string host, ushort port) @safe {
+  import s3proxy.server : openListeningSocket, listenServer;
+
+  auto socket = openListeningSocket(host, port).unwrap();
+  return listenServer(socket);
+}
+
+auto createProxy(string configFile) @safe {
+  import s3proxy.config;
+  import std.experimental.logger : globalLogLevel;
+  import std.file : readText;
+
+  auto config = loadConfig(readText(configFile)).parseConfig();
+  auto api = shared Proxy(config);
+
+  globalLogLevel = config.logging;
+  
+  return api;
 }
